174943616db39d4a793a9e65a2b9c65473a67595
SalongDb <143243056+SalongDb@users.noreply.github.com>
2026-02-08 22:47:19 +0530

    Upgrade: Added logic to get the account balance

--- src/components/ConnectForm.jsx
+++ src/components/ConnectForm.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import { ethers } from 'ethers';
+import { ethers, formatEther } from 'ethers';
 
 function ConnectForm() {
 
     const [account, setAccount] = useState();
+    const [balance, setBalance] = useState();
 
     async function connectWallet(){
         if (!window.ethereum) {
@@ -14,9 +15,13 @@ function ConnectForm() {
         try {
             const provider = new ethers.BrowserProvider(window.ethereum);
             const signer = await provider.getSigner();
+
             const address = await signer.getAddress();
+            const balance = await provider.getBalance(address);
+            const ethbalnce = formatEther(balance);
 
             setAccount(address);
+            setBalance(ethbalnce);
         } catch (error) {
             console.error(error);
         }
@@ -24,10 +29,13 @@ function ConnectForm() {
 
     return <div className="flex justify-center h-full">
         {!account ? (
-            <button onClick={()=>{connectWallet()}} >Connect wallet</button>
+            <button onClick={() =>{
+                connectWallet()
+            }} >Connect wallet</button>
         ) : (
             <div>
                 <p>Connected Account: {account}</p>
+                <p>Balance : {balance} ETH</p>
             </div>
         )}
     </div>

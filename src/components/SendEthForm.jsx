42929ca5af72f0fdd48f9c536f948997134ae818
SalongDb <143243056+SalongDb@users.noreply.github.com>
2026-02-11 21:36:19 +0530

    feat: add validation for address and amount

--- src/components/SendEthForm.jsx
+++ src/components/SendEthForm.jsx
@@ -1,27 +1,45 @@
-import { useState } from "react";
-import { parseEther } from "viem";
+import { useEffect, useState } from "react";
+import { isAddress, parseEther } from "viem";
 import { useAccount, useSendTransaction } from "wagmi";
 
 function SendEthForm() {
     const [to, setTo] = useState("");
     const [amount, setAmount] = useState("");
-    const [empty, setEmpty] = useState(false);
+    const [error, setError] = useState("");
     const { isConnected } = useAccount();
     const { data: hash, sendTransaction } = useSendTransaction();
 
+    useEffect(() => {
+        setTo("");
+        setAmount("");
+        setError("");
+    }, [isConnected]);
+
     function sendEth() {
 
-        if(!to.trim() || !amount.trim()){
-            setEmpty(true);
-
-            setTimeout(() =>{
-                setEmpty(false);
-            },5000)
-
+        if (!to.trim() || !amount.trim()) {
+            setError("All fields are required !");
             return;
         }
 
-        setEmpty(false);
+        if (!isAddress(to)) {
+            setError("Invalid Ethereum Address !")
+            return;
+        }
+
+        let parsedAmount;
+
+        try {
+            parsedAmount = parseEther(amount);
+        } catch {
+            setError("Invalid Amount Format !")
+            return;
+        }
+
+        if (Number(amount) <= 0) {
+            setError("Amount must be greater than 0 !")
+            return;
+        }
 
         sendTransaction({
             to,
@@ -39,19 +57,24 @@ function SendEthForm() {
             <div className="flex items-center justify-center flex-col">
                 <div className="flex flex-col items-center gap-3 border-1 rounded-sm px-10 py-5">
                     <div className="text-xl flex flex-col gap-2">
-                        <input onChange={(e) => {setTo(e.target.value); 
-                            setEmpty(false);
-                            }} className="bg-gray-300 px-3 rounded-sm" placeholder="To"></input>
-                        <input onChange={(e) => {setAmount(e.target.value); 
-                            setEmpty(false);
-                            }} className="bg-gray-300 px-3 rounded-sm" placeholder="Amount"></input>
+                        <input onChange={(e) => setTo(e.target.value)}
+                            onClick={(e) => setError("")}
+                            className="bg-gray-300 px-3 rounded-sm"
+                            placeholder="To" />
+                        <input onChange={(e) => setAmount(e.target.value)}
+                            onClick={(e) => setError("")}
+                            className="bg-gray-300 px-3 rounded-sm"
+                            placeholder="Amount" />
                     </div>
                     <div>
-                        <button onClick={() => { sendEth() }} className="bg-gray-800 text-white px-3 rounded-sm">Send</button>
+                        <button onClick={() => { sendEth() }}
+                            className="bg-gray-800 text-white px-3 rounded-sm">
+                            Send
+                        </button>
                     </div>
                     <div>
                         {hash && <div>Transaction Hash: {hash}</div>}
-                        {empty && <p className="text-red-600">Error ! One or both credentials missing</p>}
+                        {error && <p className="text-red-600">{error}</p>}
                     </div>
                 </div>
             </div>
